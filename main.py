b8c75345ddff02537ae5661e7c1aa33d666251a4
Martin Lüdemann <martin.luedemann78@gmail.com>
2022-07-14 11:33:44 +0200

    Text created in marktdown format as specified in stage 1

---
+++ main.py
@@ -0,0 +1,14 @@
+output_string = \
+    """
+# John Lennon
+or ***John Winston Ono Lennon*** was one of *The Beatles*.
+Here are the songs he wrote I like the most:
+
+* Imagine
+* Norwegian Wood
+* Come Together
+* In My Life
+* ~Hey Jude~ (that was McCartney)
+"""
+
+print(output_string)
