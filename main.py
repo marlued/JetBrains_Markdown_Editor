6c7c246960814f6299eba544432c5ddf452c7dbf
Martin Lüdemann <martin.luedemann78@gmail.com>
2022-07-15 11:59:15 +0200

    Basic structure of the program created. Own tests successfully completed.

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-output_string = \
-    """
-# John Lennon
-or ***John Winston Ono Lennon*** was one of *The Beatles*.
-Here are the songs he wrote I like the most:
+# initialize required variables
 
-* Imagine
-* Norwegian Wood
-* Come Together
-* In My Life
-* ~~Hey Jude~~ (that was *McCartney*)
-"""
+available_formats = 'plain bold italic header link inline-code ordered-list unordered-list new-line'.split()
+program_running = True
+choose_format = 'Choose a formatter'
+error_message = 'Unknown formatting type or command'
+help_message = 'Available formatters: plain bold italic header link inline-code ordered-list unordered-list' \
+               'new-line\nSpecial commands: !help !done'
 
-print(output_string)
+while program_running:
+
+    print(choose_format)
+    user_input = input().strip()
+
+    if user_input == '!done':
+        break
+
+    if user_input == '!help':
+        print(help_message)
+        continue
+
+    if user_input not in available_formats:
+        print(error_message)
+        continue
+
+    if user_input in available_formats:
+        continue
